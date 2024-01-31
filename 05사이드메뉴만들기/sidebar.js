172679647e4da9cc56eaef2cc4dc727b8323e245
jung4260 <min4263@hotmail.com>
2024-01-31 22:36:13 +0900

    1st home 2024-01-31

--- 05사이드메뉴만들기/sidebar.js
+++ 05사이드메뉴만들기/sidebar.js
@@ -1,2 +1,16 @@
 /* ==========좌측사이드========== */
 
+var sidenav = document.querySelector(".sidenav");
+var menuBtn = document.querySelector(".menuBtn");
+
+menuBtn.onclick = function () {
+    menuBtn.classList.toggle("active");
+    if(menuBtn.classList.contains("active")){
+        sidenav.style.display = "none";
+    }else{
+        sidenav.style.display = "block";
+    }
+}
+
+
+
