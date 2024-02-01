86b2de55c4bf1080d048bcbb5569e2c1922b83ef
jung4260 <min4263@hotmail.com>
2024-02-01 18:25:26 +0900

    6th 2024-02-01

--- 05사이드메뉴만들기/sidebar.js
+++ 05사이드메뉴만들기/sidebar.js
@@ -4,12 +4,7 @@ var sidenav = document.querySelector(".sidenav");
 var menuBtn = document.querySelector(".menuBtn");
 
 menuBtn.onclick = function () {
-    menuBtn.classList.toggle("active");
-    if(menuBtn.classList.contains("active")){
-        sidenav.style.display = "none";
-    }else{
-        sidenav.style.display = "block";
-    }
+    sidenav.classList.toggle("active");
 }
 
 
